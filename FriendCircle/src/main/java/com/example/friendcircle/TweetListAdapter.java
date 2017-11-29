6523467fc8de49fd32a990b937da5d586d9268b0
qiuqiudady <462530832@qq.com>
2017-11-29 23:23:42 +0800

    现在基本功能已经实现，还有如下问题待解决： 1. RecyclerView.OnScrollListener onScrolled中不能调用nofifyDataSetChanged，无法刷新界面； 2. 一次从内存中异步加载5条tweets，当前还是同步的； 3. 图片加载到内存的大小需要设置；

--- FriendCircle/src/main/java/com/example/friendcircle/TweetListAdapter.java
+++ FriendCircle/src/main/java/com/example/friendcircle/TweetListAdapter.java
@@ -1,9 +1,17 @@
 package com.example.friendcircle;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.support.v7.widget.GridLayout;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.TextUtils;
+import android.text.style.ForegroundColorSpan;
 import android.util.SparseArray;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
@@ -15,17 +23,34 @@ import android.widget.TextView;
 import com.example.friendcircle.bean.TweetBean;
 import com.example.friendcircle.bean.UserBean;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
 /**
  * @author yaobaocheng
- *
+ * *
  */
 public class TweetListAdapter extends RecyclerView.Adapter<TweetListAdapter.BaseViewHolder> {
     private Activity mContext;
     private LinkedList<TweetBean> mTweetList;
     private UserBean mUser;
+
+    public LinkedList<TweetBean> getmTweetList() {
+        return mTweetList;
+    }
+
+    public void setmTweetList(LinkedList<TweetBean> mTweetList) {
+        this.mTweetList = mTweetList;
+    }
+
+    public UserBean getmUser() {
+        return mUser;
+    }
+
+    public void setmUser(UserBean mUser) {
+        this.mUser = mUser;
+    }
 
     // 每条content展开/收起的状态
     private SparseArray<Integer> mTextStateList;
@@ -46,17 +71,33 @@ public class TweetListAdapter extends RecyclerView.Adapter<TweetListAdapter.Base
      */
     private static final int MAX_LINE_COUNT      = 6;
     private static final int STATE_UNKNOWN       = -1;
-    private static final int STATE_NOT_OVERFLOW  = 1;    //文本行数没有超过限定行数
+    private static final int STATE_NOT_OVERFLOW  = 1;       //文本行数没有超过限定行数
     private static final int STATE_COLLAPSED     = 2;       //文本行数超过限定行数，进行折叠
-    private static final int STATE_EXPANDED      = 3;        //文本超过限定行数，被点击全文展开
+    private static final int STATE_EXPANDED      = 3;       //文本超过限定行数，被点击全文展开
 
-    private static final int IMAGES_MAX_ROW = 3;
     private static final int IMAGES_MAX_COLUMN = 3;
 
-    public TweetListAdapter(Activity context, LinkedList<TweetBean> tweetBeans, UserBean user) {
+    public static final int LOAD_TWEETS_NUM_EACH_TIME = 5;    // load 5 tweets each time
+
+    // all images in memory, key:MD5 of url, value:Bitmap object
+    private HashMap<String, Bitmap> mBitmapSet;
+
+    public void setmBitmapSet(HashMap<String, Bitmap> mBitmapSet) {
+        this.mBitmapSet = mBitmapSet;
+    }
+
+    /**
+     * find bitmap from HashMap with url string
+     * @param url
+     * @return
+     */
+    private Bitmap getBitmap(String url) {
+        if (null == mBitmapSet || null == url) return null;
+        return mBitmapSet.get(ImageLoaderUtil.decodeMD5(url));
+    }
+
+    public TweetListAdapter(Activity context) {
         mContext = context;
-        mTweetList = tweetBeans;
-        mUser = user;
         mTextStateList = new SparseArray();
     }
 
@@ -85,19 +126,31 @@ public class TweetListAdapter extends RecyclerView.Adapter<TweetListAdapter.Base
         }
     }
 
+    /**
+     * bind user head
+     * @param holder
+     */
     private void bindHeader(final BaseViewHolder holder) {
         ImageView profile = holder.getImageView(R.id.profile);
         ImageView avatar = holder.getImageView(R.id.avatar);
         TextView nick = holder.getTextView(R.id.nick);
-        // TODO   set profile and avatar
+
+        //set profile and avatar
+        profile.setImageBitmap(getBitmap(mUser.getProfileimage()));
+        avatar.setImageBitmap(getBitmap(mUser.getAvatar()));
         nick.setText(mUser.getNick());
     }
 
+    /**
+     * bind a tweet to adapter
+     * @param holder
+     * @param position
+     */
     private void bindTweet(final BaseViewHolder holder, final int position) {
         // get Tweet object
         final int dataPosition = (hasHeader() ? position-1 : position);
         TweetBean tweet = mTweetList.get(dataPosition);
-        if (null == tweet) return;
+        if (null == tweet || !tweet.isValid()) return;
 
         final ImageView avatar = holder.getImageView(R.id.avatar);
         final TextView nick = holder.getTextView(R.id.nick);
@@ -106,69 +159,157 @@ public class TweetListAdapter extends RecyclerView.Adapter<TweetListAdapter.Base
         final GridLayout images = (GridLayout)holder.getView(R.id.images_layout);
         final LinearLayout comments = (LinearLayout)holder.getView(R.id.comments_layout);
 
-
-        // TODO
+        avatar.setImageBitmap(getBitmap(tweet.getSender().getAvatar()));
         nick.setText(tweet.getSender().getNick());
-
         bindContent(dataPosition, tweet.getContent(), content, expandOrCollapse);
-
         bindImages(images, tweet.getImages());
-
         bindComments(comments, tweet.getComments());
     }
 
+    /**
+     * bind comments to adapter
+     * @param commentsView
+     * @param comments
+     */
     private void bindComments(LinearLayout commentsView, List<TweetBean.CommentBean> comments) {
+        //remove old all
+        commentsView.removeAllViews();
         if (null == comments || comments.isEmpty()) return;
 
         for (TweetBean.CommentBean comment : comments) {
             TextView tv = new TextView(mContext);
-            tv.setText(comment.getSender().getNick() + ":" + comment.getContent());
+            float size = mContext.getResources().getDimension(R.dimen.text_size);
+            tv.setTextSize(TypedValue.COMPLEX_UNIT_PX, size);
 
-//            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams
+            // discard comment has no content or sender nick
+            if (TextUtils.isEmpty(comment.getSender().getNick()) || TextUtils.isEmpty(comment.getContent())) {
+                continue;
+            }
+            String text = comment.getSender().getNick() + ":" + comment.getContent();
 
+            // set different color for comment sender's nick
+            SpannableStringBuilder builder = new SpannableStringBuilder(text);
+            ForegroundColorSpan blueSpan = new ForegroundColorSpan(Color.BLUE);
+            ForegroundColorSpan blackSpan = new ForegroundColorSpan(Color.BLACK);
+            builder.setSpan(blueSpan, 0, comment.getSender().getNick().length()+1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            builder.setSpan(blackSpan, comment.getSender().getNick().length()+1, text.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            tv.setText(builder);
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT);
+            tv.setLayoutParams(params);
+            commentsView.addView(tv);
         }
     }
 
+    /**
+     * bind images to adapter
+     * @param imagesLayout
+     * @param images
+     */
     private void bindImages(GridLayout imagesLayout, List<TweetBean.ImagesBean> images) {
+        //remove old all
+        imagesLayout.removeAllViews();
         if (null == images || images.isEmpty()) return;
 
-        //清空子视图 防止原有的子视图影响
-        imagesLayout.removeAllViews();
-
-        // single image show
+        // a single image need to be add, now the image is not Square Image View
         if (images.size() == 1) {
-            // TODO
-            // only one image, show it fully, not grid
-            ImageView iv = new ImageView(mContext);
-            iv.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            iv.setMaxWidth(400);
-            // TODO  add image src
-            iv.setImageResource(R.drawable.profile);
-            imagesLayout.setColumnCount(1);
-            GridLayout.LayoutParams layoutParams = new GridLayout.LayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-            imagesLayout.addView(iv, layoutParams);
+            addSingleImage(imagesLayout, images.get(0));
             return;
         }
 
+        // when there are multi iamges, it's Square Image View
+        addMultiImages(imagesLayout, images);
+    }
+
+    /**
+     * add multi images to tweet, it't not more than 9.
+     * When there are 4 images, we should fill 3'rd column with invisiable view
+     * @param imagesLayout
+     * @param images
+     */
+    private void addMultiImages(GridLayout imagesLayout, List<TweetBean.ImagesBean> images) {
         int columnCount = IMAGES_MAX_COLUMN;
         imagesLayout.setColumnCount(columnCount);
 
-        //遍历集合 动态添加
+        //add all images to GridLayout
         for (int i = 0, size = images.size(); i < size; i++) {
-            GridLayout.Spec rowSpec = GridLayout.spec(i / columnCount);//行数
-            GridLayout.Spec columnSpec = GridLayout.spec(i % columnCount, 1.0f);//列数 列宽的比例 weight=1
+            // fill invisiable ImageView in 3'rd column, when the images size equals to 4
+            int specLocation = fillInvisiableView(imagesLayout, i, size);
+
+            // create a SquareImageView instance
             ImageView imageView = new SquareImageView(mContext);
             imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setImageResource(R.drawable.profile);  // TODO
-            //由于宽（即列）已经定义权重比例 宽设置为0 保证均分
+            imageView.setImageBitmap(getBitmap(images.get(i).getUrl()));
+
+            //set width to 0, using weight value
             GridLayout.LayoutParams layoutParams = new GridLayout.LayoutParams(new ViewGroup.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT));
+            GridLayout.Spec rowSpec = GridLayout.spec(specLocation / columnCount);
+            GridLayout.Spec columnSpec = GridLayout.spec(specLocation % columnCount, 1.0f);
             layoutParams.rowSpec = rowSpec;
             layoutParams.columnSpec = columnSpec;
-            layoutParams.setMargins(0, 0, 0, 0);
+            int margins = mContext.getResources().getDimensionPixelOffset(R.dimen.image_margins);
+            layoutParams.setMargins(margins, margins, margins, margins);
 
             imagesLayout.addView(imageView, layoutParams);
         }
+    }
 
+    private static final int IMAGE_COUNT_FOUR = 4;
+    private static final int IMAGE_INVISIABLE_VIEW_INDEX = 2;
+    /**
+     * fill invisiable ImageView in 3'rd column, when the images size equals to 4
+     * @param imagesLayout
+     * @param i
+     * @param size
+     * @return
+     */
+    private int fillInvisiableView(GridLayout imagesLayout, int i, int size) {
+        int specLocation = i;
+        if (IMAGE_COUNT_FOUR == size) {
+
+            // add a invisible imageView to get 1/3 weight for each one
+            if (IMAGE_INVISIABLE_VIEW_INDEX == i) {
+                ImageView placeHolder = new SquareImageView(mContext);
+                placeHolder.setVisibility(View.INVISIBLE);
+
+                GridLayout.LayoutParams placeHolderParams = new GridLayout.LayoutParams(new ViewGroup.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT));
+                GridLayout.Spec rowSpec = GridLayout.spec(0);
+                GridLayout.Spec columnSpec = GridLayout.spec(2, 1.0f);
+                placeHolderParams.rowSpec = rowSpec;
+                placeHolderParams.columnSpec = columnSpec;
+                imagesLayout.addView(placeHolder, placeHolderParams);
+            }
+
+            // add one for Spec row and column
+            if (i > 1) {
+                specLocation = i + 1;
+            }
+        }
+        return specLocation;
+    }
+
+    /**
+     * add a single image to tweet, it's not Square Image View
+     * @param imagesLayout
+     * @param imagesBean
+     */
+    private void addSingleImage(GridLayout imagesLayout, TweetBean.ImagesBean imagesBean) {
+        ImageView imageView = new ImageView(mContext);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setAdjustViewBounds(true);
+        imageView.setMaxHeight(mContext.getResources().getDimensionPixelOffset(R.dimen.max_height_single_image));
+        imageView.setMaxWidth(mContext.getResources().getDimensionPixelOffset(R.dimen.max_width_single_image));
+        imageView.setImageBitmap(getBitmap(imagesBean.getUrl()));
+
+        GridLayout.Spec rowSpec = GridLayout.spec(0);
+        GridLayout.Spec columnSpec = GridLayout.spec(0);
+        GridLayout.LayoutParams layoutParams = new GridLayout.LayoutParams
+                (new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        layoutParams.rowSpec = rowSpec;
+        layoutParams.columnSpec = columnSpec;
+
+        imagesLayout.addView(imageView, layoutParams);
     }
 
     /**
@@ -179,6 +320,12 @@ public class TweetListAdapter extends RecyclerView.Adapter<TweetListAdapter.Base
      * @param expandOrCollapse
      */
     private void bindContent(final int dataPosition, String contentString, final TextView content, final TextView expandOrCollapse) {
+        if (TextUtils.isEmpty(contentString)) {
+            content.setText("");
+            expandOrCollapse.setText("");
+            return;
+        }
+
         int state = mTextStateList.get(dataPosition, STATE_UNKNOWN);
 
         //如果该item是第一次初始化，则取获取文本的行数
@@ -284,5 +431,65 @@ public class TweetListAdapter extends RecyclerView.Adapter<TweetListAdapter.Base
         public Button getButton(int id) {
             return findView(id);
         }
+    }
+
+    public abstract static class EndLessOnScrollListener extends  RecyclerView.OnScrollListener{
+
+        //声明一个LinearLayoutManager
+        private LinearLayoutManager mLinearLayoutManager;
+
+        //当前页，从0开始
+        private int currentPage = 0;
+
+        //已经加载出来的Item的数量
+        private int totalItemCount;
+
+        //主要用来存储上一个totalItemCount
+        private int previousTotal = 0;
+
+        //在屏幕上可见的item数量
+        private int visibleItemCount;
+
+        //在屏幕可见的Item中的第一个
+        private int firstVisibleItem;
+
+        //是否正在上拉数据
+        private boolean loading = true;
+
+        public EndLessOnScrollListener(LinearLayoutManager linearLayoutManager) {
+            this.mLinearLayoutManager = linearLayoutManager;
+        }
+
+        @Override
+        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+            super.onScrolled(recyclerView, dx, dy);
+
+            visibleItemCount = recyclerView.getChildCount();
+            totalItemCount = mLinearLayoutManager.getItemCount();
+            firstVisibleItem = mLinearLayoutManager.findFirstVisibleItemPosition();
+            if(loading){
+                //Log.d("wnwn","firstVisibleItem: " +firstVisibleItem);
+                //Log.d("wnwn","totalPageCount:" +totalItemCount);
+                //Log.d("wnwn", "visibleItemCount:" + visibleItemCount)；
+
+                if(totalItemCount > previousTotal){
+                    //说明数据已经加载结束
+                    loading = false;
+                    previousTotal = totalItemCount;
+                }
+            }
+            //这里需要好好理解
+            if (!loading && totalItemCount-visibleItemCount <= firstVisibleItem){
+                currentPage ++;
+                onLoadMore(currentPage);
+                loading = true;
+            }
+        }
+
+        /**
+         * 提供一个抽闲方法，在Activity中监听到这个EndLessOnScrollListener
+         * 并且实现这个方法
+         * */
+        public abstract void onLoadMore(int currentPage);
     }
 }
